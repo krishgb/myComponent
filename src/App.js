cb34e26c882cf8a7c84aba88fbbbc2939395c072
krish <krishchella1918@outlook.com>
2021-08-04 18:22:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, createRef } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent headerText="Say with me">
+        <h1>GO BEYOND PLUS ULTRA</h1>
+      </MyComponent>
     </div>
-  );
+  )
 }
 
-export default App;
+class MyComponent extends Component {
+  constructor(props) {
+
+    // calling super constructor with props
+    super(props)
+
+    // instead of stringrefs(this.refs) createRefs can be used
+    this.myComponent = createRef()
+
+    // set the default internal state
+    this.state = {
+      clicks: 0
+    }
+  }
+  componentDidMount() {
+    // this.refs.myComponentDiv.addEventListener('click', this.clickHandler)
+    this.myComponent.current.addEventListener('click', this.clickHandler)
+  }
+  componentWillUnmount() {
+    // this.refs.myComponentDiv.removeEventListener('click', this.clickHandler)
+    this.myComponent.current.removeEventListener('click', this.clickHandler)
+  }
+
+  // make it as an arrow function to get rid of "this"
+  clickHandler = () => {
+    this.setState({
+      // clicks: this.clicks + 1 - should be this.state.click
+      clicks: this.state.clicks + 1
+    })
+  }
+
+  render() {
+    const { children, headerText } = this.props
+    return (
+      <div className="my-component" ref={this.myComponent}>
+        <h2>My Component ({this.state.clicks} clicks)</h2>
+        <h3>{headerText}</h3>
+        {children}
+      </div>
+    )
+  }
+}
